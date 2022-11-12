4ed167cc73fd58f2ffe75e7d23a4a09f0f1abd2f
thesecretpathllc <118075180+thesecretpathllc@users.noreply.github.com>
2022-11-12 17:34:01 -0500

    Updates

---
+++ precache-manifest.5b2cb00cb808f17bda3c842bd46bcc04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8297e302c0dfe3c89cdf930a9cf718a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03ce15201667ef27ef3e",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "682600d26e7a6180fb5e",
+    "url": "/static/js/2.037f93bd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.037f93bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ce15201667ef27ef3e",
+    "url": "/static/js/main.4e6fa092.chunk.js"
+  },
+  {
+    "revision": "7372e9576b5c3fb021ea",
+    "url": "/static/js/runtime-main.5578a860.js"
+  }
+]);
